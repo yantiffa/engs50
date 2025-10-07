901fd739a2eee3df7ae4f2404e7557377099f21a
Tiffany Yan <Tingwen.Yan.UG@dartmouth.edu>
2025-10-07 11:06:46 -0400

    updated hash.c

--- module3/hash/hash.c
+++ module3/hash/hash.c
@@ -96,4 +96,26 @@ void hclose(hashtable_t *htp) {
 	free(hash->queues);
 	free(hash);
 }
-	
+
+/* hput -- puts an entry into a hash table under designated key
+	 returns 0 for success; non-zero otherwise
+ */
+int32_t hput(hashtable_t *htp, void *ep, const char *key, int keylen);
+
+/* happly -- applies a function to every entry in hash table */
+void happly(hashtable_t *htp, void (*fn)(void *ep));
+
+/* hsearch -- searches for an entry under a designated key using a
+	 designated search fn -- returns a pointer to the entry or NULL if
+	 not found
+*/
+void *hsearch(hashtable_t *htp, bool (*searchfn)(void *elementp, const void *searchkeyp),const char *key,int32_t keylen);
+
+/* hremove -- removes and returns an entry under a designated key
+	 using a designated search fn -- returns a pointer to the entry or
+	 NULL if not found
+*/
+void *hremove(hashtable_t *htp,
+              bool (*searchfn)(void *elementp, const void *searchkeyp),
+              const char *key,
+              int32_t keylen);
