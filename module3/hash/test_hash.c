87511f0d46c02048400bba8d890435557b5d570c
Tiffany Yan <Tingwen.Yan.UG@dartmouth.edu>
2025-10-07 21:56:24 -0400

    added test for fopen and fclose

---
+++ module3/hash/test_hash.c
@@ -0,0 +1,26 @@
+/* 
+ * test_hash.c --- 
+ * 
+ * Author: Tingwen Yan
+ * Created: 10-07-2025
+ * Version: 1.0
+ * 
+ * Description: 
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include "queue.h"
+#include "hashtable.h"
+
+void test_hopen_hclose() {
+	printf("Testing hopen/hclose...\n");
+	hashtable_t *hash = hopen(100);
+	assert(hash != NULL);
+	assert(hash->size == 100);
+	assert(hash->queues != NULL);
+	hclose(hash);
+	printf("Passed basic hopen/hclose\n\n");
+}
