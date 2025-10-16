45096c9bc9a9381e971ca686453d345c8716a447
Yuri Naidu <yurinaidu.yn@gmail.com>
2025-10-16 15:29:53 +0000

    Updated crawler

--- tse/crawler/crawler.c
+++ tse/crawler/crawler.c
@@ -5,15 +5,6 @@
 #include "../webpage.h"
 #include "queue.h"
 
-// Ensure the definition of struct webpage is available
-// If webpage.h does not define struct webpage, add its definition here:
-typedef struct webpage {
-  char *url;                               // url of the page
-  char *html;                              // html code of the page
-  size_t html_len;                         // length of html code
-  int depth;                               // depth of crawl
-} webpage_t;
-
 void print_webpage(void* elementp) {
     webpage_t *page = (webpage_t*)elementp;
     printf("URL: %s, Depth: %d\n", webpage_getURL(page), webpage_getDepth(page));
@@ -22,17 +13,16 @@ void print_webpage(void* elementp) {
 int main() {
 	webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
 	if(!webpage_fetch(page)) {
-	printf("Failed to fetch webpage\n");
-	webpage_delete(page);
-	exit(EXIT_FAILURE);
-}
+		printf("Failed to fetch webpage\n");
+		webpage_delete(page);
+		exit(EXIT_FAILURE);
+	}
 	//step3: with queue
 	queue_t * queue = qopen();
 	int pos = 0;
 	char *result;
 	while ((pos = webpage_getNextURL(page, pos, &result)) > 0) {
 		if(IsInternalURL(result)){
-			int current_depth = webpage_getDepth(page);
 			webpage_t *child_page = webpage_new(result, 1, NULL);
 			qput(queue, child_page);
 		}
