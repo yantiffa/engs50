b2d0c112ac699c33b6afc4bfc37b3c4868f477ae
Tiffany Yan <Tingwen.Yan.UG@dartmouth.edu>
2025-10-21 21:15:28 -0400

    step 2 done

--- tse/indexer/indexer.c
+++ tse/indexer/indexer.c
@@ -8,9 +8,51 @@
  * Description: 
  * 
  */
+#include <ctype.h>
 #include <stdio.h>
+#include "pageio.h"
+
+int NormalizeWord(char *word) {
+	// if word is empty
+	if (word == NULL){
+		return 0;
+	}
+
+	// check for length
+	int lens = strlen(word);
+	if (lens < 3){
+		return 0;
+	}
+	for (int i = 0; i < lens; i++) {
+		if (!isalpha(word[i])) {
+			return 0;
+		}
+		word[i] = tolower(word[i]);
+	}
+	return 1;
+}
+
+
+
+	
+	
 int main() {
-	printf("Hello World!\n");
+	webpage_t *page = pageload(1, "../pages");
+	if (page == NULL) {
+		fprintf(stderr, "error!\n");
+		return 1;
+	}
+	int pos = 0;
+	char *word;
+
+	while ((pos = webpage_getNextWord(page, pos, &word)) > 0) {
+		if (NormalizeWord(word) == 1) {
+			printf("%s\n", word);
+		}
+		free(word);
+	}
+	webpage_delete(page);
+	
 	return 0;
 }
 
