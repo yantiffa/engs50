d8d4495077e8713466826a0dc254aac259c30629
Tiffany Yan <Tingwen.Yan.UG@dartmouth.edu>
2025-10-21 20:00:56 -0400

    made indexer.c

---
+++ tse/indexer.c
@@ -0,0 +1,16 @@
+/* 
+ * indexer.c --- 
+ * 
+ * Author: Tingwen Yan
+ * Created: 10-21-2025
+ * Version: 1.0
+ * 
+ * Description: 
+ * 
+ */
+
+int main() {
+	printf("Hello World!\n");
+	return 0;
+}
+
