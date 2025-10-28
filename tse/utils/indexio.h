11002fde65bfc25fecb5f679eeda846eceb9f02f
yantiffa <yantiffa@grinnell.edu>
2025-10-28 10:33:19 -0400

    Create indexio.h

---
+++ tse/utils/indexio.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stddef.h>
+
+typedef struct Index Index;
+
+Index *index_new(void);
+
+int index_add_posting(Index *idx, const char *word, int docid, int count);
+
+void index_free(Index *idx);
+
+int indexsave(const char *indexnm, const Index *idx);
+
+Index *indexload(const char *indexnm);
