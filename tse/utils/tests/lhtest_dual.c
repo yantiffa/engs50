472d010018d6b0b81d58ce477a8cfbabc3015aab
Tiffany Yan <Tingwen.Yan.UG@dartmouth.edu>
2025-11-10 13:14:30 -0500

    test

---
+++ tse/utils/tests/lhtest_dual.c
@@ -0,0 +1,18 @@
+/* 
+ * lhtest_dual.c --- 
+ * 
+ * Author: Tingwen Yan
+ * Created: 11-10-2025
+ * Version: 1.0
+ * 
+ * Description: 
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "lhash.h"
+#include <string.h>
+
+#define N_PER_THREAD 10000
+
